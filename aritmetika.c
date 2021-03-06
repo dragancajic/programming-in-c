1a0dddf5d5ee500a6692256b3bba378c1767e9c8
Dragan Cajic <cajic_dragan@yahoo.com>
2021-03-06 18:42:35 +0100

    division by zero is not allowed!

--- aritmetika.c
+++ aritmetika.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 // The #define preprocessor directive
-#define BLANK_CHARACTER ' '
+//#define BLANK_CHARACTER ' '
 
 /**
  * @author: Драган Ћајић
@@ -16,7 +16,7 @@ int main(void) {
 	// konstante
 	const int A1 = 17; // const is a type qualifier
 	const int B1 = 8;
-	//const char BLANK_CHARACTER = ' ';
+	const char BLANK_CHARACTER = ' ';
 
 	// promjenljive
 	int a = 0;
@@ -48,8 +48,57 @@ int main(void) {
 	printf("\n"); // prelazak u sljedeci red
 	printf("\n");
 
+	// P R O M J E N LJ I V E
+    printf(" >>> Unesite cjelobrojne vrijednosti za a i b\n");
+    printf("{brojeve razdvojte pritiskom na SPACE ili ENTER}: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    // test
+    //printf("%d\n", a);
+    //printf("%d\n", b);
+
+    printf("\n"); // prelazak u sljedeci red
+	printf("\n");
+
+	// B1 -- ispis u jednom redu
+	if (b == 0)
+		printf("Dijeljenje nulom nije dozvoljeno!");
+	else {
+		printf("%d", a+b);
+		printf("%c", BLANK_CHARACTER);
+		printf("%d", a-b);
+		printf("%c", BLANK_CHARACTER);
+		printf("%d", a*b);
+		printf("%c", BLANK_CHARACTER);
+		printf("%d", a/b);
+		printf("%c", BLANK_CHARACTER);
+		printf("%d", a%b);
+	}
+
+	printf("\n"); // prelazak u sljedeci red
+	printf("\n");
+
+	// B2 -- ispis na zasebnoj liniji
+	printf("    zbir a + b = ");
+	printf("%d\n", a + b);
+	printf(" razlika a - b = ");
+	printf("%d\n", a - b);
+	printf("proizvod a * b = ");
+	printf("%d\n", a * b);
+
+	// provjera na dijeljenje nulom (b != 0, ne smije biti b == 0)
+	if (b == 0)
+		printf("Dijeljenje nulom nije dozvoljeno!\n");
+	else {
+		printf("kolicnik a : b je ");
+		printf("%d\n", a / b);
+		printf(" ostatak a : b je ");
+		printf("%d\n", a % b);
+	}
+
 	// ispis na konzolu
-	printf("Dobar dan! Kako ste?\n");
+	//printf("\nDobar dan! Kako ste?\n");
 
 	return 0;
 }
